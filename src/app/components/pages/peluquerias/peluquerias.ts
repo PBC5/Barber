fa501995a2510b603d6f38d4dfcfe07cabacc24a
PBC5 <160127358+PBC5@users.noreply.github.com>
2025-12-02 13:00:41 +0100

    Estructura inicial

---
+++ src/app/components/pages/peluquerias/peluquerias.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-peluquerias',
+  imports: [],
+  templateUrl: './peluquerias.html',
+  styleUrl: './peluquerias.scss',
+})
+export class Peluquerias {
+
+}
---
+++ src/app/components/ui/c-panel/c-panel.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-c-panel',
+  imports: [],
+  templateUrl: './c-panel.html',
+  styleUrl: './c-panel.scss',
+})
+export class CPanel {
+
+}
